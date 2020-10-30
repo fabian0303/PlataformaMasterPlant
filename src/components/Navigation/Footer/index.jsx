cb9fe2915cc25fc467c85a3dddc75c154c0ea825
Fabian Oyarce <faoyarce15@alumnos.utalca.cl>
2020-10-30 11:03:58 -0300

    Proyecto Base + Login Firebase + Control de Usuarios

---
+++ src/components/Navigation/Footer/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import classes from './Footer.module.scss';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container-fluid">
+        <div className={classNames('level', classes.level)}>
+          <div className="level-left">
+            <div className="level-item">
+              © 2020 <span>&nbsp; MasterPlant</span>
+            </div>
+          </div>
+          <div className={classNames('level-right', classes.levelRight)}>
+            <div className="level-item">
+              <div className="logo">
+                <a href="https://www.masterplant.cl/">
+                  <img
+                    src="https://www.masterplant.cl/images/logo.jpg"
+                    alt="masterplant.cl"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
